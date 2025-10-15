dfa789619534556f6079576e14fb92d61e9c669e
beamiter <beamiter@163.com>
2025-10-15 17:58:55 +0800

    fix cpp

--- src/ts_hl/queries/cpp_symbols.scm
+++ src/ts_hl/queries/cpp_symbols.scm
@@ -1,16 +1,39 @@
-; functions
+; 函数定义（自由函数）
 (function_definition
   declarator: (function_declarator
-                declarator: (identifier) @symbol.function))
+    declarator: (identifier) @symbol.function))
 
-; class/struct
-(class_specifier name: (type_identifier) @symbol.class)
-(struct_specifier name: (type_identifier) @symbol.struct)
+; 成员函数定义（类外定义：Class::method）
+(function_definition
+  declarator: (function_declarator
+    declarator: (qualified_identifier
+      name: (identifier) @symbol.method)))
 
-; namespace
-(namespace_definition name: (namespace_identifier) @symbol.namespace)
+; 成员函数声明（类内原型）
+(field_declaration
+  declarator: (function_declarator
+    declarator: (field_identifier) @symbol.method))
 
-; methods inside class
-(field_declaration (function_declarator declarator: (field_identifier) @symbol.method))
-; Note: depending on grammar, methods can also appear as function_definition under class body
-(method_definition name: (field_identifier) @symbol.method)
+; 函数原型（自由函数原型）
+(declaration
+  declarator: (function_declarator
+    declarator: (identifier) @symbol.function))
+
+; 类 / 结构体 / 枚举
+(class_specifier
+  name: (type_identifier) @symbol.class)
+
+(struct_specifier
+  name: (type_identifier) @symbol.struct)
+
+(enum_specifier
+  name: (type_identifier) @symbol.enum)
+
+; 命名空间
+(namespace_definition
+  name: (identifier) @symbol.namespace)
+
+; 变量声明（只抓 identifier，忽略复杂声明）
+(declaration
+  declarator: (init_declarator
+    declarator: (identifier) @symbol.variable))
